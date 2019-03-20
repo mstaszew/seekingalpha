519c7d6116b43e087fac5486b458816c263f86b3
mike stash <mst.rocking@gmail.com>
2019-03-20 23:00:37 +0200

    bootstrap  in the twig file

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,11 +1,17 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     public function index()
+    {
+
+        return $this->render('base.html.twig', []);
+    }
+    public function indexJson()
     {
         $number = random_int(0, 100);
 
